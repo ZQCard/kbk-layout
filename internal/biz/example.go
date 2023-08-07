2cdfe40d3ea6a9090fc5465866cbe14f8a1dcce7
qi.zhou <44586474@qq.com>
2023-08-08 01:37:20 +0800

    update

--- internal/biz/example.go
+++ internal/biz/example.go
@@ -81,7 +81,7 @@ func (suc *ExampleUsecase) DeleteExample(ctx context.Context, example *domain.Ex
 func (suc *ExampleUsecase) RecoverExample(ctx context.Context, example *domain.Example) error {
 	count, _ := suc.repo.GetExampleCount(ctx, map[string]interface{}{
 		"id":         example.Id,
-		"id_deleted": true,
+		"is_deleted": true,
 	})
 	if count == 0 {
 		return v1.ErrorRecordNotFound("数据不存在")
